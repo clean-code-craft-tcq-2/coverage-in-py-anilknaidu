cc7919c90804dc2b361d6d0e6317629c62128422
Naidu Anil Kumar (RBEI/EPV32-EC) <AnilKumar.Naidu@in.bosch.com>
2022-02-20 15:59:52 +0530

    Test commit

--- typewise_alert.py
+++ typewise_alert.py
@@ -1,8 +1,7 @@
 batteryChar = {'coolingType':"PASSIVE_COOLING"}
 
 class send_alert:
-  
-  send_alert_dcn = {'TO_CONTROLLER':"send_to_contoller", 'TO_EMAIL':"send_to_email"}
+  send_alert_dcn = {'TO_CONTROLLER':"send_to_controller", 'TO_EMAIL':"send_to_email"}
   breach_dcn = {'TOO_LOW':'Hi, the temperature is too low','TOO_HIGH':'Hi, the temperature is too high'}
 
   def send_to_controller(self,breachType):
@@ -15,17 +14,18 @@ class send_alert:
 
 class battery_check_and_alert(send_alert):
   coolingType_dcn = {'PASSIVE_COOLING':(0,35),'HI_ACTIVE_COOLING':(0,45),'MED_ACTIVE_COOLING':(0,40)}
-  def __init__(self,alertTarget, batteryChar, temperatureInC,recepient):
+  def __init__(self,alertTarget, batteryChar, temperatureInC,*recepient):
     self.alertTarget = alertTarget
     self.coolingType = batteryChar["coolingType"]
     self.temperatureInC = temperatureInC
-    self.recepient = recepient
+    if(alertTarget == "TO_EMAIL"):
+      self.recepient = recepient[0]
 
   def classify_temperature_breach(self):
     return self.infer_breach(self.coolingType_dcn[self.coolingType][0], self.coolingType_dcn[self.coolingType][1])
   
   def infer_breach(self,lowerLimit,upperLimit ):
-    if self.temperatureInC in range(lowerLimit,upperLimit):
+    if self.temperatureInC in range(lowerLimit,upperLimit+1):
       return 'NORMAL'
     if self.temperatureInC > upperLimit:
       return 'TOO_HIGH'
@@ -36,5 +36,5 @@ class battery_check_and_alert(send_alert):
     getattr(self,self.send_alert_dcn[self.alertTarget])(breachType)
   
 
-myAlerter = battery_check_and_alert("TO_EMAIL",batteryChar,45,"a.b@c.com")
+myAlerter = battery_check_and_alert("TO_EMAIL",batteryChar,45,"a.b.c@bosch.com")
 myAlerter.check_and_alert()
